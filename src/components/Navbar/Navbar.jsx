df2a82504a374db29086e5c6918c036676d57d4c
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-06-16 16:10:36 -0500

    fixed nav links not working on small screen sizes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,9 @@
 
 import React, { useState } from 'react'
 import './Navbar.css'
-import {BiMenuAltRight} from 'react-icons/bi'
-import {RxCross2} from 'react-icons/rx'
-import {motion, useMotionValueEvent, useScroll} from 'framer-motion';
+import { BiMenuAltRight } from 'react-icons/bi'
+import { RxCross2 } from 'react-icons/rx'
+import { motion, useMotionValueEvent, useScroll } from 'framer-motion';
 import { Link } from 'react-scroll'
 
 const Navbar = () => {
@@ -19,75 +19,83 @@ const Navbar = () => {
         }
     })
 
-  return (
-    <div className={`n-wrapper ${navStyle}`}>
-        {/* desktop version */}
-        <div className="container">
-            <div className="n-container">
-                {/* left side */}
-                <div className="n-logo">
-                    <span>FutureWave Capital</span>
-                </div>
-
-                {/* right side */}
-                <div className="n-right">
-                    <div className="n-menu">
-                        <Link to="wwd-wrapper" spy={true} smooth={true} >
-                            <span>What we do</span>
-                        </Link>
-                        <Link to="hiw-wrapper" spy smooth offset={100} >
-                            <span>How it works</span>
-                        </Link>
-                        <Link to="wwi-wrapper" spy smooth >
-                            <span>Who we invest in</span>
-                        </Link>
-                        <Link to="t-wrapper" spy smooth offset={100} >
-                            <span>Testimonials</span>
-                        </Link>
+    return (
+        <div className={`n-wrapper ${navStyle}`}>
+            {/* desktop version */}
+            <div className="container">
+                <div className="n-container">
+                    {/* left side */}
+                    <div className="n-logo">
+                        <span>FutureWave Capital</span>
                     </div>
-                    {/* <div className="fund-button">
+
+                    {/* right side */}
+                    <div className="n-right">
+                        <div className="n-menu">
+                            <Link to="wwd-wrapper" spy={true} smooth={true} >
+                                <span>What we do</span>
+                            </Link>
+                            <Link to="hiw-wrapper" spy smooth offset={100} >
+                                <span>How it works</span>
+                            </Link>
+                            <Link to="wwi-wrapper" spy smooth >
+                                <span>Who we invest in</span>
+                            </Link>
+                            <Link to="t-wrapper" spy smooth offset={100} >
+                                <span>Testimonials</span>
+                            </Link>
+                        </div>
+                        {/* <div className="fund-button">
                         Get Funded
                     </div> */}
+                    </div>
                 </div>
             </div>
-        </div>
 
-        {/* mobile version */}
-        <div className="nm-container">
-            {/* logo */}
-            <span>FutureWave Capital</span>
+            {/* mobile version */}
+            <div className="nm-container">
+                {/* logo */}
+                <span>FutureWave Capital</span>
 
-            {/* menu icon */}
-            {
-                !mobileMenuOpened ? 
-                <BiMenuAltRight
-                size={30}
-                onClick={() => setMobileMenuOpened(true)} 
-                style={{cursor:"pointer"}}
-                /> : 
-                <RxCross2 
-                size={30}
-                onClick={() => setMobileMenuOpened(false)}
-                style={{cursor:"pointer"}}
-                />
-            }
+                {/* menu icon */}
+                {
+                    !mobileMenuOpened ?
+                        <BiMenuAltRight
+                            size={30}
+                            onClick={() => setMobileMenuOpened(true)}
+                            style={{ cursor: "pointer" }}
+                        /> :
+                        <RxCross2
+                            size={30}
+                            onClick={() => setMobileMenuOpened(false)}
+                            style={{ cursor: "pointer" }}
+                        />
+                }
 
-            {/* mobile menu */}
-            <div className="nm-menu"
-            style={{transform: mobileMenuOpened && "translateX(0%)"}}
-            >
-                <span href="what-we-do">What we do</span>
-                <span>How it works</span>
-                <span>Who we invest in</span>
-                <span>Testimonials</span>
-                {/* <div className="m-funded-button">
+                {/* mobile menu */}
+                <div className="nm-menu"
+                    style={{ transform: mobileMenuOpened && "translateX(0%)" }}
+                >
+                    <Link to="wwd-wrapper" spy={true} smooth={true} onClick={() => setMobileMenuOpened(false)}>
+                        <span>What we do</span>
+                    </Link>
+                    <Link to="hiw-wrapper" spy smooth offset={100} onClick={() => setMobileMenuOpened(false)}>
+                        <span>How it works</span>
+                    </Link>
+                    <Link to="wwi-wrapper" spy smooth onClick={() => setMobileMenuOpened(false)}>
+                        <span>Who we invest in</span>
+                    </Link>
+                    <Link to="t-wrapper" spy smooth offset={100} onClick={() => setMobileMenuOpened(false)}>
+                        <span>Testimonials</span>
+                    </Link>
+                    {/* <div className="m-funded-button">
                     Get Funded
                 </div> */}
-            </div>
+                </div>
 
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
