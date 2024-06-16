a9d67b484cbcc040ba061a7e56b41289539ce7cb
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-06-16 16:23:27 -0500

    updated email box and hero content

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,15 +4,22 @@ import React from 'react'
 import './Hero.css'
 import EmailBox from '../EmailBox/EmailBox'
 import { HeroData } from '@/src/utils/data'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
+
+const generatePastelColor = () => {
+    const r = Math.floor((Math.random() * 127) + 127);
+    const g = Math.floor((Math.random() * 127) + 127);
+    const b = Math.floor((Math.random() * 127) + 127);
+    return `rgb(${r}, ${g}, ${b})`;
+}
 
 const Hero = () => {
 
     const variants = (delay) => ({
-        initial : {
+        initial: {
             y: "18rem"
         },
-        animate : {
+        animate: {
             y: "0rem",
             transition: {
                 type: "spring",
@@ -24,9 +31,9 @@ const Hero = () => {
     })
 
     const imgVariants = () => ({
-        initial : {
+        initial: {
             y: "18rem"
-        }, 
+        },
         animate: {
             y: "0rem",
             transition: {
@@ -37,74 +44,74 @@ const Hero = () => {
         }
     })
 
-  return (
-    <div className="h-wrapper">
-        <div className="container">
-            <img src="hero/arrow.png" alt="" className="h-arrow" />
-            <div className="h-container">
-                {/* left Side */}
-                <div className="h-left">
-                    <div className="image-row">
-                        {
-                            HeroData.slice(0, 3).map((person, i)=> (
-                                <div className='person-pill' key={i}>
-                                    <motion.div 
-                                    initial={"initial"}
-                                    animate={"animate"}
-                                    variants={variants()}
-                                    style={{backgroundColor: person.bg}}
-                                    className="person-pill-bg">
-                                        <motion.img 
-                                        initial={"initial"}
-                                        animate={"animate"}
-                                        variants={imgVariants()}
-                                        src={person.src} 
-                                        alt={person.src} />
-                                    </motion.div>
-                                </div>
-                            ))
-                        }
-                    </div>
-                    <div className="image-row">
-                        {
-                            HeroData.slice(3, 6).map((person, i)=> (
-                                <div className='person-pill' key={i}>
-                                    <motion.div 
-                                    initial={"initial"}
-                                    animate={"animate"}
-                                    variants={variants()}
-                                    style={{backgroundColor: person.bg}}
-                                    className="person-pill-bg">
-                                        <motion.img 
-                                        initial={"initial"}
-                                        animate={"animate"}
-                                        variants={imgVariants()}
-                                        src={person.src} 
-                                        alt={person.src} />
-                                    </motion.div>
-                                </div>
-                            ))
-                        }
-                    </div>
-                </div>
-
-                {/* right Side */}
-                <div className="h-right">
-                    <div className="h-title">
-                        <span>Redefine How</span>
-                        <span>You Grow your</span>
-                        <span>Digital Business</span>
+    return (
+        <div className="h-wrapper">
+            <div className="container">
+                <img src="hero/arrow.png" alt="" className="h-arrow" />
+                <div className="h-container">
+                    {/* left Side */}
+                    <div className="h-left">
+                        <div className="image-row">
+                            {
+                                HeroData.slice(0, 3).map((person, i) => (
+                                    <div className='person-pill' key={i}>
+                                        <motion.div
+                                            initial={"initial"}
+                                            animate={"animate"}
+                                            variants={variants()}
+                                            style={{ backgroundColor: generatePastelColor() }}
+                                            className="person-pill-bg">
+                                            <motion.div
+                                                initial={"initial"}
+                                                animate={"animate"}
+                                                variants={imgVariants()}
+                                                style={{ width: "100%", height: "100%" }}
+                                            />
+                                        </motion.div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                        <div className="image-row">
+                            {
+                                HeroData.slice(3, 6).map((person, i) => (
+                                    <div className='person-pill' key={i}>
+                                        <motion.div
+                                            initial={"initial"}
+                                            animate={"animate"}
+                                            variants={variants()}
+                                            style={{ backgroundColor: generatePastelColor() }}
+                                            className="person-pill-bg">
+                                            <motion.div
+                                                initial={"initial"}
+                                                animate={"animate"}
+                                                variants={imgVariants()}
+                                                style={{ width: "100%", height: "100%" }}
+                                            />
+                                        </motion.div>
+                                    </div>
+                                ))
+                            }
+                        </div>
                     </div>
 
-                    <div className="h-description">
-                        Revenue based funding and execution support designed for early stage founders
+                    {/* right Side */}
+                    <div className="h-right">
+                        <div className="h-title">
+                            <span>Redefine How</span>
+                            <span>You Grow your</span>
+                            <span>Digital Business</span>
+                        </div>
+
+                        <div className="h-description">
+                            Revenue based funding and execution support designed for early stage founders
+                        </div>
+                        <EmailBox />
                     </div>
-                    <EmailBox />
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Hero
